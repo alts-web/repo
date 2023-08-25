9102b63de4968341a2a6b8ec82088a69016b269a
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-25 12:13:41 -0500

    Update client.js

--- static/client.js
+++ static/client.js
@@ -1,17 +1,17 @@
    var user = window.localStorage.getItem('user');
-        var validUser = "abhayisan3gr0";
-        if (user && user === validUser) {
    var password = window.localStorage.getItem('password');
+        var validUser = "abhayisan3gr0";
         var validPassword = "abhayisan3gr0";
         if (password && password === validPassword) {
             document.body.style.display = 'block';
         } else {
             var input = prompt('Enter the password:');
             if (input === validPassword) {
+                window.localStorage.setItem('user', input);
                 window.localStorage.setItem('password', input);
                 document.body.style.display = 'block';
             } else {
-                alert('Invalid password.');
+                alert('Invalid password or user.');
                 window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
             }
         }
