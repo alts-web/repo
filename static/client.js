cdadbacf1c73285f3d4958734186c673a6107dc7
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-25 18:22:35 -0500

    Update client.js

--- static/client.js
+++ static/client.js
@@ -8,15 +8,15 @@
             var input = prompt('Enter your User:');
             if (input === validUser) {
             var input = prompt('Enter your Password:');
-                if (input === validPassword) {
+                if (input === validPassword) 
                 window.localStorage.setItem('user', input);
                 window.localStorage.setItem('password', input);
                 document.body.style.display = 'block';
-            } 
+             
             }
-                else {
-                alert('Invalid password or user.');
-                window.location.href = 'https://launchpad.classlink.com/rrisd'
+            else {
+            alert('Invalid password or user.');
+            window.location.href = 'https://launchpad.classlink.com/rrisd'
             }
            
             
