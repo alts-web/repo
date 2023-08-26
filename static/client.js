aa4d29fdc1675982aa57c75abbd68ba1463752e6
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-25 19:43:12 -0500

    Update client.js

--- static/client.js
+++ static/client.js
@@ -1,24 +1,21 @@
+
 var user = window.localStorage.getItem('user');
 var password = window.localStorage.getItem('password');
 var validUsers = ["Hrithvik", "user2", "user3"]; // array of valid users
 var validPasswords = ["Vuppala", "password2", "password3"]; // array of valid passwords
-var isValidLogin = validUsers.includes(user) && validPasswords.includes(password);
 
-if (user && password && isValidLogin) {
-    document.body.style.display = 'block';
-} else {
-    var inputUser = prompt('Enter your User:');
+if (validUsers.includes(user)) {
     var inputPassword = prompt('Enter your Password:');
-    var isValidInput = validUsers.includes(inputUser) && validPasswords.includes(inputPassword);
-
-    if (isValidInput) {
-        window.localStorage.setItem('user', inputUser);
+  
+    if (validPasswords.includes(inputPassword)) {
         window.localStorage.setItem('password', inputPassword);
         document.body.style.display = 'block';
     } else {
-        alert('Invalid user or password.');
-        window.location.href = 'https://launchpad.classlink.com/rrisd';
+        alert('Invalid password.');
     }
+} else {
+    alert('Not a valid user.');
+    window.location.href = 'https://launchpad.classlink.com/rrisd';
 }
 
 if (document.body.style.display !== 'block') {
