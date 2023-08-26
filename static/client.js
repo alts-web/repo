7e0faf4cd2e9a161259a315449218e151ae2d8c1
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-25 20:06:38 -0500

    Update client.js

--- static/client.js
+++ static/client.js
@@ -4,9 +4,11 @@ var validUsers = ["Hrithvik", "user2", "user3"]; // array of valid users
 var validPasswords = ["Vuppala", "password2", "password3"]; // array of valid passwords
 
 if (validUsers.includes(user)) {
-    var inputPassword = prompt('Enter your Password:');
+    var inputUserPassword = prompt('Enter your Username and Password (separated by a space):');
+    var [inputUser, inputPassword] = inputUserPassword.split(' ');
   
     if (validPasswords.includes(inputPassword)) {
+        window.localStorage.setItem('user', inputUser);
         window.localStorage.setItem('password', inputPassword);
         document.body.style.display = 'block';
     } else {
@@ -15,25 +17,24 @@ if (validUsers.includes(user)) {
         document.body.style.display = 'none';
     }
 } else {
-    var inputUser = prompt('Enter your User:');
+  var inputUserPassword = prompt('Enter your Username and Password (separated by a space):');
+  var [inputUser, inputPassword] = inputUserPassword.split(' ');
   
-    if (validUsers.includes(inputUser)) {
-        var inputPassword = prompt('Enter your Password:');
-      
-        if (validPasswords.includes(inputPassword)) {
-            window.localStorage.setItem('user', inputUser);
-            window.localStorage.setItem('password', inputPassword);
-            document.body.style.display = 'block';
-        } else {
-            alert('Invalid password.');
-            window.location.href = 'https://launchpad.classlink.com/rrisd';
-            document.body.style.display = 'none';
-        }
+  if (validUsers.includes(inputUser)) {
+    if (validPasswords.includes(inputPassword)) {
+        window.localStorage.setItem('user', inputUser);
+        window.localStorage.setItem('password', inputPassword);
+        document.body.style.display = 'block';
     } else {
-        alert('Invalid user.');
+        alert('Invalid password.');
         window.location.href = 'https://launchpad.classlink.com/rrisd';
         document.body.style.display = 'none';
     }
+  } else {
+    alert('Invalid user.');
+    window.location.href = 'https://launchpad.classlink.com/rrisd';
+    document.body.style.display = 'none';
+  }
 }
 
 if (document.body.style.display !== 'block') {
