263936b2d9af7927b4ceaddf2d35a4a2aed824c5
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-25 19:18:52 -0500

    Update client.js

--- static/client.js
+++ static/client.js
@@ -1,6 +1,6 @@
    var user = window.localStorage.getItem('user');
    var password = window.localStorage.getItem('password');
-        var validUser = "Hrithvik" = "Aarav";
+        var validUser = "Hrithvik";
         var validPassword = "Vuppala";
         if (user && user === validUser) {
             document.body.style.display = 'block';
