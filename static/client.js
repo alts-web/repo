c75165b73424a552563eaa5933df7133b323bf28
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-25 20:04:23 -0500

    Update client.js

--- static/client.js
+++ static/client.js
@@ -4,28 +4,33 @@ var validUsers = ["Hrithvik", "user2", "user3"]; // array of valid users
 var validPasswords = ["Vuppala", "password2", "password3"]; // array of valid passwords
 
 if (validUsers.includes(user)) {
-    var inputUser = prompt('Enter your Username:');
     var inputPassword = prompt('Enter your Password:');
   
-    if (validPasswords.includes(inputPassword) && validUsers.includes(inputUser)) {
-        window.localStorage.setItem('user', inputUser);
+    if (validPasswords.includes(inputPassword)) {
         window.localStorage.setItem('password', inputPassword);
         document.body.style.display = 'block';
     } else {
-        alert('Invalid username or password.');
+        alert('Invalid password.');
         window.location.href = 'https://launchpad.classlink.com/rrisd';
         document.body.style.display = 'none';
     }
 } else {
-    var inputUser = prompt('Enter your Username:');
-    var inputPassword = prompt('Enter your Password:');
+    var inputUser = prompt('Enter your User:');
   
-    if (validPasswords.includes(inputPassword) && validUsers.includes(inputUser)) {
-        window.localStorage.setItem('user', inputUser);
-        window.localStorage.setItem('password', inputPassword);
-        document.body.style.display = 'block';
+    if (validUsers.includes(inputUser)) {
+        var inputPassword = prompt('Enter your Password:');
+      
+        if (validPasswords.includes(inputPassword)) {
+            window.localStorage.setItem('user', inputUser);
+            window.localStorage.setItem('password', inputPassword);
+            document.body.style.display = 'block';
+        } else {
+            alert('Invalid password.');
+            window.location.href = 'https://launchpad.classlink.com/rrisd';
+            document.body.style.display = 'none';
+        }
     } else {
-        alert('Invalid username or password.');
+        alert('Invalid user.');
         window.location.href = 'https://launchpad.classlink.com/rrisd';
         document.body.style.display = 'none';
     }
