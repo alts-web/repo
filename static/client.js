6db7678be6a82e15e274090e929c1fac5a80f38e
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-27 12:30:06 -0500

    Update client.js

--- static/client.js
+++ static/client.js
@@ -11,7 +11,8 @@ if (!validUsers.includes(user)) {
         var inputPassword = prompt('Enter your Password:');
         if (validPasswords.includes(inputPassword)) {
             window.localStorage.setItem('user', user);
-            window.localStorage.setItem('password', inputPassword);
+            window.localStorage.setItem('password', inputPassword
+            document.body.style.display = 'block';
         } else {
             alert('Invalid Password.');
             window.location.href = 'https://launchpad.classlink.com/rrisd';
