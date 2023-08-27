12650822e19b19e840990027215cf73480b6d033
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-26 22:13:25 -0500

    Update aboutblank.js

--- static/aboutblank.js
+++ static/aboutblank.js
@@ -1,38 +1,40 @@
 let inFrame;
 
 try {
-    inFrame = window !== top;
+  inFrame = window !== top;
 } catch (e) {
-    inFrame = true;
+  inFrame = true;
 }
 
 // Cloaking Code
 if (!inFrame) {
-    const popup = open("about:blank", "_blank");
+  const popup = window.open("about:blank", "_blank");
   if (!popup || popup.closed) {
-  alert("Allow popups and redirects to hide this from showing up in your history.")
+    alert("Allow popups and redirects to hide this from showing up in your history.");
   } else {
-        const doc = popup.document;
-        const iframe = doc.createElement("iframe");
-        const style = iframe.style;
-        const link = doc.createElement("link");
+    const doc = popup.document;
+    const iframe = doc.createElement("iframe");
+    const style = iframe.style;
+    const link = doc.createElement("link");
+    const script = doc.createElement("script");
 
-        const name = localStorage.getItem("name") || "Inbox - RoundRock ISD Mail";
-        const icon = localStorage.getItem("icon") || "/favicon.ico";
-        
-        doc.title = name;
-        link.rel = "icon";
-        link.href = icon;
-        
-        script.src = "/client.js";
-        iframe.src = location.href
-        style.position = "fixed";
-        style.top = style.bottom = style.left = style.right = 0;
-        style.border = style.outline = "none";
-        style.width = style.height = "100%";
+    const name = localStorage.getItem("name") || "Inbox - RoundRock ISD Mail";
+    const icon = localStorage.getItem("icon") || "/favicon.ico";
+    
+    doc.title = name;
+    link.rel = "icon";
+    link.href = icon;
+    
+    script.src = "/client.js";
+    iframe.src = location.href;
+    style.position = "fixed";
+    style.top = style.bottom = style.left = style.right = 0;
+    style.border = style.outline = "none";
+    style.width = style.height = "100%";
 
-        doc.head.appendChild(link);
-        doc.body.appendChild(iframe);
-        location.replace("https://roundrockisd.schoology.com/home");
-    }
+    doc.head.appendChild(link);
+    doc.body.appendChild(iframe);
+    doc.body.appendChild(script);
+    location.replace("https://roundrockisd.schoology.com/home");
+  }
 }
