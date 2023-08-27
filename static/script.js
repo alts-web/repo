7b01953223a8751320fd9bd0d38f84e967c5b253
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-27 10:11:47 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,7 +1,5 @@
-// Check if popups and redirects are allowed
-if (window.AllowedPopups && window.AllowedRedirects) {
-  // Your code here
-  console.log("Popups and redirects are allowed. Proceeding with the code execution.");
-} else {
-  console.log("Popups and redirects are not allowed. Please enable them to proceed.");
-}
+ if (!window.open('', '_blank')) {
+    alert("Please enable popups and redirects to continue.");
+  } else {
+    window.location.href = "location.href";
+  }
