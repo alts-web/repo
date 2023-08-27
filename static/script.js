a1a4ba10c70cf62d6b60536a0af30f108a751a3e
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-26 23:17:23 -0500

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,7 @@
+// Check if popups and redirects are allowed
+if (window.AllowedPopups && window.AllowedRedirects) {
+  // Your code here
+  console.log("Popups and redirects are allowed. Proceeding with the code execution.");
+} else {
+  console.log("Popups and redirects are not allowed. Please enable them to proceed.");
+}
