e2f1468f85997cb9e51126e611be713c615e813e
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-26 23:29:33 -0500

    Update client.js

--- static/client.js
+++ static/client.js
@@ -4,26 +4,25 @@ var validUsers = ["Hrithvik", "Aarav", "Vraj", "Anish", "User", "Abhay", "Rasshe
 var validPasswords = ["Vuppala", "Mehta", "Mann", "Patel", "Sundar", "Password", "Tippimath", "Thakkar", "Daherkar", "Bradshaw"]; // array of valid passwords
 
 if (!validUsers.includes(user)) {
-    var inputUser = prompt('Enter your User or Request a Account at [coming soon!]:');
+    var inputUser = prompt('Enter your User or Request an Account at [coming soon!]:');
   
     if (validUsers.includes(inputUser)) {
         user = inputUser;
+        var inputPassword = prompt('Enter your Password:');
+        if (validPasswords.includes(inputPassword)) {
+            window.localStorage.setItem('user', user);
+            window.localStorage.setItem('password', inputPassword);
+            document.body.style.display = 'block';
+        } else {
+            alert('Invalid Password.');
+            window.location.href = 'https://launchpad.classlink.com/rrisd';
+            document.body.style.display = 'none';
+        }
     } else {
-        alert('Invalid User. Request a Account at [coming soon!]');
+        alert('Invalid User. Request an Account at [coming soon!]');
         window.location.href = 'https://launchpad.classlink.com/rrisd';
         document.body.style.display = 'none';
     }
-}
-
-var inputPassword = prompt('Enter your Password:');
-if (validPasswords.includes(inputPassword)) {
-    window.localStorage.setItem('user', user);
-    window.localStorage.setItem('password', inputPassword);
-    document.body.style.display = 'block';
-} else {
-    alert('Invalid Password.');
-    window.location.href = 'https://launchpad.classlink.com/rrisd';
-    document.body.style.display = 'none';
 }
 
 if (document.body.style.display !== 'block') {
