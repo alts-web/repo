a691662a7bf97d0d57c947f2a9b53b21f1f4ace9
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-27 10:38:19 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,1 @@
-        function checkFeatures() {
-            if (!window.open || !window.location.assign) {
-                document.body.style.display = "none";
-            } else {
-                document.body.style.display = "block";
-            }
-        }
 
-        function showAlert() {
-            if (!window.open || !window.location.assign) {
-                alert("Please enable popups and redirects to continue.");
-            }
-        }
