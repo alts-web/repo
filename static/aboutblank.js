7fdea5c02133fb8714bd5ab1cd8aa42579da5629
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-29 10:40:58 -0500

    Update aboutblank.js

--- static/aboutblank.js
+++ static/aboutblank.js
@@ -19,12 +19,11 @@ if (!inFrame) {
 
     const name = localStorage.getItem("name") || "Inbox - RoundRock ISD Mail";
     const icon = localStorage.getItem("icon") || "/favicon.ico";
-    
+
     doc.title = name;
     link.rel = "icon";
     link.href = icon;
-    
-    script.src = "/client.js";
+
     iframe.src = location.href;
     style.position = "fixed";
     style.top = style.bottom = style.left = style.right = 0;
@@ -33,7 +32,16 @@ if (!inFrame) {
 
     doc.head.appendChild(link);
     doc.body.appendChild(iframe);
+
+    // Wait for the client.js code to finish executing before redirecting
+    function redirectToURL() {
+      location.replace("https://roundrockisd.schoology.com/home");
+    }
+
+    // Call the client.js code and pass the redirectToURL function as a callback
+    script.src = "/client.js";
+    script.onload = redirectToURL;
+
     doc.body.appendChild(script);
-    location.replace("https://roundrockisd.schoology.com/home");
   }
 }
