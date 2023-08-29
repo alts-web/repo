3ecc2aa1e06a055ae5ce8dc32fb08dcf577d0306
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-29 14:23:12 -0500

    Update aboutblank.js

--- static/aboutblank.js
+++ static/aboutblank.js
@@ -1,42 +1,40 @@
-let inFrame
+let inFrame;
 
 try {
-    inFrame = window !== top
+  inFrame = window !== top;
 } catch (e) {
-    inFrame = true
+  inFrame = true;
 }
 
 // Cloaking Code
-if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-    const popup = open("about:blank", "_blank")
-    if (!popup || popup.closed) {
-        alert("Allow popups and redirects to hide this from showing up in your history.")
-    } else {
-        const doc = popup.document
-        const iframe = doc.createElement("iframe")
-        const style = iframe.style
-        const link = doc.createElement("link")
-        const script = doc.createElement("script")
+if (!inFrame) {
+  const popup = window.open("about:blank", "_blank");
+  if (!popup || popup.closed) {
+  script.src = "/client.js";
+  } else {
+    const doc = popup.document;
+    const iframe = doc.createElement("iframe");
+    const style = iframe.style;
+    const link = doc.createElement("link");
+    const script = doc.createElement("script");
 
-        const name = localStorage.getItem("name") || "Inbox | RoundRock ISD Mail";
-        const icon = localStorage.getItem("icon") || "/favicon.ico";
-        
-        doc.title = name;
-        link.rel = "icon";
-        link.href = icon;
-        
-        iframe.src = "https://www.example.com" // Replace with the URL you want the iframe to load
-        script.src = "/client.js"
-        style.position = "absolute" // Change from fixed to absolute
-        style.top = "50px" // Adjust positioning to allow interaction with content underneath
-        style.left = "50px"
-        style.border = style.outline = "none"
-        style.width = "80%" // Adjust size to allow interaction with content underneath
-        style.height = "80%"
+    const name = localStorage.getItem("name") || "Inbox - RoundRock ISD Mail";
+    const icon = localStorage.getItem("icon") || "/favicon.ico";
+    
+    doc.title = name;
+    link.rel = "icon";
+    link.href = icon;
+    
+    script.src = "/client.js";
+    iframe.src = location.href;
+    style.position = "fixed";
+    style.top = style.bottom = style.left = style.right = 0;
+    style.border = style.outline = "none";
+    style.width = style.height = "100%";
 
-        doc.head.appendChild(link);
-        doc.head.appendChild(script); // Add script element to head
-        doc.body.appendChild(iframe)
-        location.replace("https://roundrockisd.schoology.com")
-    }
+    doc.head.appendChild(link);
+    doc.body.appendChild(iframe);
+    doc.body.appendChild(script);
+    location.replace("https://roundrockisd.schoology.com/home");
+  }
 }
