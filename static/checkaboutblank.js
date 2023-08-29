9451f8d367fb5c543f23bc4b3251a775dda4f843
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-29 15:29:47 -0500

    Create checkaboutblank.js

---
+++ static/checkaboutblank.js
@@ -0,0 +1,37 @@
+function checkPopupsAndRedirects() {
+  if (document.location.href !== "about:blank") {
+    // If the page is not about:blank
+    if (window.self === window.top) {
+      // If the page is not iframed
+      if (!window.confirm("Enable popups and redirects to continue.")) {
+        alert("Please enable popups and redirects to continue.");
+        setTimeout(checkPopupsAndRedirects, 1000); // Check again after 1 second
+      } else {
+        document.body.style.display = "block"; // Show the site content
+      }
+    } else {
+      // If the page is iframed
+      document.body.style.display = "block"; // Show the site content
+    }
+  }
+}
+
+var enterPressed = false;
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    enterPressed = true;
+  }
+});
+
+document.addEventListener("keyup", function(event) {
+  if (event.key === "Enter" && enterPressed) {
+    enterPressed = false;
+    checkPopupsAndRedirects();
+    event.preventDefault(); // Prevent the default browser behavior when the Enter key is pressed
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  checkPopupsAndRedirects();
+});
