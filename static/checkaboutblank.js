3006ba0086921e450643c0c5cffdd2b635643885
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-29 16:10:49 -0500

    Update checkaboutblank.js

--- static/checkaboutblank.js
+++ static/checkaboutblank.js
@@ -3,11 +3,15 @@ function checkPopupsAndRedirects() {
     // If the page is not about:blank
     if (window.self === window.top) {
       // If the page is not iframed
+      var popupInterval = setInterval(function() {
         if (!window.confirm("Enable popups and redirects to continue.")) {
           alert("Please enable popups and redirects to continue.");
-          setTimeout(checkPopupsAndRedirects, 1000); // Check again after 1 second
-          document.body.style.display = 'none'; // Hides the site content
-      }
+        } else {
+          clearInterval(popupInterval); // Stop showing the popup
+          document.body.style.display = "block"; // Show the site content
+        }
+      }, 1000); // Show the popup every 1 second
+      document.body.style.display = "none"; // Hide the site content
     } else {
       // If the page is iframed
       document.body.style.display = "block"; // Show the site content
