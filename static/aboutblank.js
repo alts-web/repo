c4db2664a3c2518beadfa27ff666243788d709d4
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-29 22:14:43 -0500

    Update aboutblank.js

--- static/aboutblank.js
+++ static/aboutblank.js
@@ -24,7 +24,7 @@ if (!inFrame) {
   link.href = icon;
 
   script.src = "/client.js"
-  iframe.src = location.href; // Replace with the URL you want the iframe to load
+  iframe.src = "https://s-web.cyclic.app"; // Replace with the URL you want the iframe to load
   style.position = "fixed";
   style.top = style.bottom = style.left = style.right = 0;
   style.border = style.outline = "none";
