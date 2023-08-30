187620a9dcb683bac10481e9a8af6903a2aea65a
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-29 23:44:12 -0500

    Update checkaboutblank.js

--- static/checkaboutblank.js
+++ static/checkaboutblank.js
@@ -1,40 +1,24 @@
-   function showSiteContent() {
+function showSiteContent() {
   document.body.style.display = "block"; // Show the site content
 }
 
 function checkPopupsAndRedirects() {
-  if ((window.self === window.top || document.location.href === "about:blank") && !window.localStorage.getItem("popupShown")) {
-    // If the page is not iframed or is the about:blank page, and the popup has not been shown before
+  if (window.self === window.top || document.location.href === "about:blank") {
+    // If the page is not iframed or is the about:blank page
     if (!window.confirm("Enable popups and redirects to continue.")) {
       alert("Please enable popups and redirects to continue.");
-      window.localStorage.setItem("popupShown", true); // Mark the popup as shown
       setTimeout(checkPopupsAndRedirects, 1000); // Check again after 1 second
       document.body.style.display = "none";
     } else {
       showSiteContent();
     }
   } else {
-    // If the page is iframed or the popup has been shown before
+    // If the page is iframed
     showSiteContent();
   }
 }
 
-var enterPressed = false;
-
-document.addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    enterPressed = true;
-  }
-});
-
-document.addEventListener("keyup", function(event) {
-  if (event.key === "Enter" && enterPressed) {
-    enterPressed = false;
-    checkPopupsAndRedirects();
-    event.preventDefault(); // Prevent the default browser behavior when the Enter key is pressed
-  }
-});
-
 document.addEventListener("DOMContentLoaded", function() {
-  checkPopupsAndRedirects();
+  setInterval(checkPopupsAndRedirects, 1000); // Repeat the check every second
 });
+
