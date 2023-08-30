52c61281c19ac76e683bed786b71a6fd89afa7cf
Skydiver <126831858+Skydiver-Web@users.noreply.github.com>
2023-08-29 23:40:19 -0500

    Update checkaboutblank.js

--- static/checkaboutblank.js
+++ static/checkaboutblank.js
@@ -1,39 +1,40 @@
-    function showSiteContent() {
-    document.body.style.display = "block"; // Show the site content
+   function showSiteContent() {
+  document.body.style.display = "block"; // Show the site content
+}
+
+function checkPopupsAndRedirects() {
+  if ((window.self === window.top || document.location.href === "about:blank") && !window.localStorage.getItem("popupShown")) {
+    // If the page is not iframed or is the about:blank page, and the popup has not been shown before
+    if (!window.confirm("Enable popups and redirects to continue.")) {
+      alert("Please enable popups and redirects to continue.");
+      window.localStorage.setItem("popupShown", true); // Mark the popup as shown
+      setTimeout(checkPopupsAndRedirects, 1000); // Check again after 1 second
+      document.body.style.display = "none";
+    } else {
+      showSiteContent();
     }
+  } else {
+    // If the page is iframed or the popup has been shown before
+    showSiteContent();
+  }
+}
 
-    function checkPopupsAndRedirects() {
-      if (window.self === window.top || document.location.href === "about:blank") {
-        // If the page is not iframed or is the about:blank page
-        if (!window.confirm("Enable popups and redirects to continue.")) {
-          alert("Please enable popups and redirects to continue.");
-          setTimeout(checkPopupsAndRedirects, 1000); // Check again after 1 second
-          document.body.style.display = "none";
-        } else {
-          showSiteContent();
-        }
-      } else {
-        // If the page is iframed
-        showSiteContent();
-      }
-    }
+var enterPressed = false;
 
-    var enterPressed = false;
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    enterPressed = true;
+  }
+});
 
-    document.addEventListener("keydown", function(event) {
-      if (event.key === "Enter") {
-        enterPressed = true;
-      }
-    });
+document.addEventListener("keyup", function(event) {
+  if (event.key === "Enter" && enterPressed) {
+    enterPressed = false;
+    checkPopupsAndRedirects();
+    event.preventDefault(); // Prevent the default browser behavior when the Enter key is pressed
+  }
+});
 
-    document.addEventListener("keyup", function(event) {
-      if (event.key === "Enter" && enterPressed) {
-        enterPressed = false;
-        checkPopupsAndRedirects();
-        event.preventDefault(); // Prevent the default browser behavior when the Enter key is pressed
-      }
-    });
-
-    document.addEventListener("DOMContentLoaded", function() {
-      checkPopupsAndRedirects();
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  checkPopupsAndRedirects();
+});
