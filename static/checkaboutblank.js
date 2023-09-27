3ffa4a3a0fda6860fec3525a4e6483d0a2991a6c
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-09-27 00:09:50 -0500

    Update checkaboutblank.js

--- static/checkaboutblank.js
+++ static/checkaboutblank.js
@@ -1,12 +1,21 @@
+let checkEnabled = true;
+
 function checkPopupsAndRedirects() {
+  if (!checkEnabled) {
+    return;
+  }
+
   const popupWindow = window.open("https://skydiver-web.cyclic.app", "_blank");
-  
-  setTimeout(function() {
+
+  setTimeout(function () {
     if (!popupWindow || popupWindow.closed || popupWindow.innerWidth === 0) {
       // Popup is blocked or closed, show the popup message
-      const popupResult = window.confirm("Enable popups and redirects in your browser settings, then please click 'OK' to continue.");
+      const popupResult = window.confirm(
+        "Enable popups and redirects in your browser settings, then please click 'OK' to continue."
+      );
       if (popupResult) {
         // User clicked 'OK', check again
+        checkEnabled = false; // Disable further checks
         checkPopupsAndRedirects();
       } else {
         // User clicked 'Cancel' or closed the popup, keep showing the popup
