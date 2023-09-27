21109d99c5d31ed911cb8464d332c92fc329950b
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-09-26 23:45:26 -0500

    Update checkaboutblank.js

--- static/checkaboutblank.js
+++ static/checkaboutblank.js
@@ -1,9 +1,22 @@
+function loadSiteInBackground() {
+  const iframe = document.createElement('iframe');
+  iframe.src = "https://skydiver-web.cyclic.app"; // Replace with your site's URL
+  iframe.style.display = "none"; // Hide the iframe
+  document.body.appendChild(iframe);
+
+  iframe.addEventListener("load", function () {
+    // When the iframe is fully loaded, it means the site has loaded in the background
+    document.body.removeChild(iframe); // Remove the hidden iframe
+  });
+}
+
 function showPopup() {
-  const confirmationResult = window.confirm("Enable popups and redirects, then please click 'OK' to reload the page.");
+  const confirmationResult = window.confirm("Enable popups and redirects, then please click 'OK' to continue.");
   if (confirmationResult) {
     // User clicked 'OK', enable popups and redirects
     localStorage.setItem('popupFlag', 'enabled');
-    location.reload(); // Reload the page
+    // You can load the site in the background here
+    loadSiteInBackground();
   } else {
     // User clicked 'Cancel' or closed the popup, keep showing the popup
     setTimeout(showPopup, 1000); // Show the popup again after 1 second
@@ -15,12 +28,6 @@ function checkPopupsAndRedirects() {
   if (!popupFlag) {
     // Popups and redirects are not enabled, show the popup
     showPopup();
-  } else {
-    // Popups and redirects are enabled
-    // Execute the script '/aboutblank.js'
-    const script = document.createElement('script');
-    script.src = '/aboutblank.js'; // Replace with the correct path to your script
-    document.head.appendChild(script);
   }
 }
 
