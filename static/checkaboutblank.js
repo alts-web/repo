a1c89ed2fc7cc8ed9b5e78f192dd7b87ae496cdb
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-09-27 00:05:51 -0500

    Update checkaboutblank.js

--- static/checkaboutblank.js
+++ static/checkaboutblank.js
@@ -1,25 +1,19 @@
 function checkPopupsAndRedirects() {
-  const popupFlag = localStorage.getItem('popupFlag');
-  const popupsEnabled = (window.innerWidth > 0) || (window.innerHeight > 0);
-
-  if (!popupFlag) {
-    if (popupsEnabled) {
-      // Popups are enabled, set the flag and reload the page
-      localStorage.setItem('popupFlag', 'enabled');
-      location.reload();
-    } else {
-      // User hasn't enabled popups and redirects yet, show the popup
+  const popupWindow = window.open("https://skydiver-web.cyclic.app", "_blank");
+  
+  setTimeout(function() {
+    if (!popupWindow || popupWindow.closed || popupWindow.innerWidth === 0) {
+      // Popup is blocked or closed, show the popup message
       const popupResult = window.confirm("Enable popups and redirects in your browser settings, then please click 'OK' to continue.");
-      if (!popupResult) {
+      if (popupResult) {
+        // User clicked 'OK', check again
+        checkPopupsAndRedirects();
+      } else {
         // User clicked 'Cancel' or closed the popup, keep showing the popup
         setTimeout(checkPopupsAndRedirects, 1000); // Show the popup again after 1 second
       }
     }
-  }
+  }, 1000); // Check after 1 second
 }
 
-const popupFlag = localStorage.getItem('popupFlag');
-if (!popupFlag) {
-  // Popups and redirects are not enabled, show the popup continuously
-  checkPopupsAndRedirects();
-}
+checkPopupsAndRedirects();
