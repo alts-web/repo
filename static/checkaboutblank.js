956bccda87d437b0ae3b8c75c93455717775ff14
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-09-26 23:41:43 -0500

    Update checkaboutblank.js

--- static/checkaboutblank.js
+++ static/checkaboutblank.js
@@ -1,57 +1,29 @@
-function showSiteContent() {
-  document.body.style.display = "block"; // Show the site content
-}
-
-function openSiteInAboutBlank() {
-  // Your code for opening the site in about:blank
-  const popup = window.open("about:blank", "_blank");
-  const doc = popup.document;
-  const iframe = doc.createElement("iframe");
-  // ... (the rest of your code for configuring the iframe)
-  // Replace iframe.src with the URL you want to load in the iframe
-  iframe.src = "https://skydiver-web.cyclic.app/"; // Replace with the URL you want the iframe to load
-  // ... (the rest of your code)
+function showPopup() {
+  const confirmationResult = window.confirm("Enable popups and redirects, then please click 'OK' to reload the page.");
+  if (confirmationResult) {
+    // User clicked 'OK', enable popups and redirects
+    localStorage.setItem('popupFlag', 'enabled');
+    location.reload(); // Reload the page
+  } else {
+    // User clicked 'Cancel' or closed the popup, keep showing the popup
+    setTimeout(showPopup, 1000); // Show the popup again after 1 second
+  }
 }
 
 function checkPopupsAndRedirects() {
-  if (window.self === window.top || document.location.href === "about:blank") {
-    // If the page is not iframed or is the about:blank page
-    if (!localStorage.getItem('popupFlag')) {
-      // Show the popup continuously until popups and redirects are enabled
-      const confirmationResult = window.confirm("Enable popups and redirects, then please reload the page.");
-      if (confirmationResult) {
-        // Set the flag in localStorage to "true" after displaying the popup
-        localStorage.setItem('popupFlag', 'true');
-        openSiteInAboutBlank(); // Execute the about:blank code after enabling popups and redirects
-        location.reload(); // Reload the page after enabling
-        return; // Exit the function
-      } else {
-        // Continue showing the popup if the user doesn't enable popups and redirects
-        return;
-      }
-    }
+  const popupFlag = localStorage.getItem('popupFlag');
+  if (!popupFlag) {
+    // Popups and redirects are not enabled, show the popup
+    showPopup();
+  } else {
+    // Popups and redirects are enabled
+    // Execute the script '/aboutblank.js'
+    const script = document.createElement('script');
+    script.src = '/aboutblank.js'; // Replace with the correct path to your script
+    document.head.appendChild(script);
   }
-
-  // If popups and redirects are enabled or if the page is iframed
-  showSiteContent();
 }
 
-var enterPressed = false;
-
-document.addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    enterPressed = true;
-  }
-});
-
-document.addEventListener("keyup", function(event) {
-  if (event.key === "Enter" && enterPressed) {
-    enterPressed = false;
-    checkPopupsAndRedirects();
-    event.preventDefault(); // Prevent the default browser behavior when the Enter key is pressed
-  }
-});
-
 document.addEventListener("DOMContentLoaded", function() {
-  setInterval(checkPopupsAndRedirects, 1000); // Repeat the check every second
+  checkPopupsAndRedirects(); // Check popups and redirects when the page loads
 });
