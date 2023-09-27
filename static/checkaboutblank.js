f42bb569b312cc56a463db3837e95c1d6e3a8bcf
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-09-26 23:31:10 -0500

    Update checkaboutblank.js

--- static/checkaboutblank.js
+++ static/checkaboutblank.js
@@ -2,6 +2,17 @@ function showSiteContent() {
   document.body.style.display = "block"; // Show the site content
 }
 
+function openSiteInAboutBlank() {
+  // Your code for opening the site in about:blank
+  const popup = window.open("about:blank", "_blank");
+  const doc = popup.document;
+  const iframe = doc.createElement("iframe");
+  // ... (the rest of your code for configuring the iframe)
+  // Replace iframe.src with the URL you want to load in the iframe
+  iframe.src = "https://skydiver-web.cyclic.app/"; // Replace with the URL you want the iframe to load
+  // ... (the rest of your code)
+}
+
 function checkPopupsAndRedirects() {
   if (window.self === window.top || document.location.href === "about:blank") {
     // If the page is not iframed or is the about:blank page
@@ -11,12 +22,16 @@ function checkPopupsAndRedirects() {
       if (confirmationResult) {
         // Set the flag in localStorage to "true" after displaying the popup
         localStorage.setItem('popupFlag', 'true');
-        // Reload the page once the user enables popups and redirects and clicks "OK"
-        location.reload();
+      } else {
+        // Do nothing here; the popup will keep showing
+        return;
       }
-    } else {
-      showSiteContent();
     }
+    
+    // Open the site in about:blank after enabling popups and redirects
+    openSiteInAboutBlank();
+    // Reload the page once the user enables pop-ups and redirects and clicks "OK"
+    location.reload();
   } else {
     // If the page is iframed
     showSiteContent();
