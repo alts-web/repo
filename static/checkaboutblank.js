72966b1223a2541033d5261cd39f1b8e4755c84b
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-09-26 23:56:29 -0500

    Update checkaboutblank.js

--- static/checkaboutblank.js
+++ static/checkaboutblank.js
@@ -1,28 +1,24 @@
-function checkPopupsAndRedirects() {
-  const popupFlag = localStorage.getItem('popupFlag');
-  if (!popupFlag) {
-    // Popups and redirects are not enabled, show the popup
-    const confirmationResult = window.confirm("Enable popups and redirects in your browser settings, then please click 'OK' to continue.");
-    if (confirmationResult) {
-      // User clicked 'OK', check if popups and redirects are enabled
-      const popupsEnabled = (window.innerWidth > 0) || (window.innerHeight > 0);
-      if (popupsEnabled) {
-        // Popups are enabled, set the flag and reload the page
-        localStorage.setItem('popupFlag', 'enabled');
-        location.reload();
-      } else {
-        // Popups are still not enabled, keep showing the popup
-        setTimeout(checkPopupsAndRedirects, 1000); // Show the popup again after 1 second
-      }
+function showPopup() {
+  const confirmationResult = window.confirm("Enable popups and redirects in your browser settings, then please click 'OK' to continue.");
+  if (confirmationResult) {
+    // User clicked 'OK', check if popups and redirects are enabled
+    const popupsEnabled = (window.innerWidth > 0) || (window.innerHeight > 0);
+    if (popupsEnabled) {
+      // Popups are enabled, set the flag and reload the page
+      localStorage.setItem('popupFlag', 'enabled');
+      location.reload();
     } else {
-      // User clicked 'Cancel' or closed the popup, keep showing the popup
-      setTimeout(checkPopupsAndRedirects, 1000); // Show the popup again after 1 second
+      // Popups are still not enabled, keep showing the popup
+      setTimeout(showPopup, 1000); // Show the popup again after 1 second
     }
+  } else {
+    // User clicked 'Cancel' or closed the popup, keep showing the popup
+    setTimeout(showPopup, 1000); // Show the popup again after 1 second
   }
 }
 
 const popupFlag = localStorage.getItem('popupFlag');
 if (!popupFlag) {
-  // Popups and redirects are not enabled, show the popup
-  checkPopupsAndRedirects();
+  // Popups and redirects are not enabled, show the popup continuously
+  showPopup();
 }
