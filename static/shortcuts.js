e59df471bea7b7719ac0d9f85eeb695d95ece23f
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-10-30 14:15:02 -0500

    Update shortcuts.js

--- static/shortcuts.js
+++ static/shortcuts.js
@@ -1,7 +1,22 @@
-// shortcuts.js
-window.addEventListener("keydown", function(event) {
-  if (event.ctrlKey && event.keyCode === 84) {
-    event.preventDefault();
-    newTab('ht://newtab');
-  }
+window.addEventListener("load", function() {
+  // Add the keyboard listener to the current tab.
+  window.addEventListener("keydown", function(event) {
+    if (event.ctrlKey && event.keyCode === 84) {
+      event.preventDefault();
+      newTab('ht://newtab');
+    }
+  });
 });
+
+function newTab(url) {
+  // Create a new tab.
+  var tab = window.open(url, '_blank');
+
+  // Add the keyboard listener to the new tab.
+  tab.addEventListener("keydown", function(event) {
+    if (event.ctrlKey && event.keyCode === 84) {
+      event.preventDefault();
+      newTab('ht://newtab');
+    }
+  });
+}
