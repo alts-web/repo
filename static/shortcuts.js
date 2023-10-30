ef87530406add5d7a47aeaff146972b43e15fbec
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-10-30 12:54:45 -0500

    Update shortcuts.js

--- static/shortcuts.js
+++ static/shortcuts.js
@@ -1,5 +1,5 @@
 // shortcuts.js
-document.addEventListener("keydown", function(event) {
+window.addEventListener("keydown", function(event) {
   if (event.ctrlKey && event.keyCode === 84) {
     event.preventDefault();
     newTab('ht://newtab');
