a4fc20eeea207825721355b3e63570f01f5a772c
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-11-20 00:44:19 -0600

    Update rewriteURL.js

--- static/rewriteURL.js
+++ static/rewriteURL.js
@@ -1,11 +1,12 @@
-// Get the current URL
-const currentURL = window.location.href;
+window.addEventListener('load', function() {
+  // Get the current URL
+  const currentURL = window.location.href;
 
-// Check if the current URL ends with "/"
-if (currentURL.endsWith('/')) {
-  // Remove the trailing "/"
-  const newURL = currentURL.slice(0, currentURL.length - 1);
+  // Check if the current URL ends with "/"
+  if (currentURL.endsWith('/')) {
+    // Remove the trailing "/"
+    const newURL = currentURL.slice(0, currentURL.length - 1);
 
-  // Rewrite the URL
-  window.location.href = newURL;
-}
+    // Rewrite the URL
+    window.location.href = newURL;
+  }
