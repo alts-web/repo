6b3d14e7ce9abdc3065db9396b8ceb65d6ba9450
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-11-20 03:45:55 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
-import path from 'path';
-
-// Check if the `path` module is installed
-const isPathInstalled = typeof path === 'object' && typeof path.join === 'function';
-
-if (!isPathInstalled) {
-  // Install the `path` module if it's not installed
-  try {
-    require('child_process').execSync('npm install path');
-    console.log('The `path` module has been installed successfully.');
-  } catch (error) {
-    console.error('Failed to install the `path` module:', error);
-  }
-}
-
-import createBareServer from
- 
-'@tomphttp/bare-server-node';
-import { fileURLToPath } from
- 
-"url";
-import { createServer as createHttpsServer } from
- 
-"node:https";
-import { createServer as createHttpServer } from
- 
-"node:http";
-import { readFileSync, existsSync } from
- 
-"node:fs";
-import serveStatic from
- 
-"serve-static";
+import createBareServer from '@tomphttp/bare-server-node';
+import { fileURLToPath } from "url";
+import { createServer as createHttpsServer } from "node:https";
+import { createServer as createHttpServer } from "node:http";
+import { readFileSync, existsSync } from "node:fs";
+import serveStatic from "serve-static";
 
 // The following message MAY NOT be removed
 console.log("Hypertabs\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
@@ -38,53 +11,38 @@ console.log("Hypertabs\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is
 const bare = createBareServer("/bare/");
 const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), { fallthrough: false });
 var server;
-if (existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
+if(existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
   server = createHttpsServer({
     key: readFileSync("../ssl/key.pem"),
-    cert: readFileSync("../ssl/cert.pem"),
+    cert: readFileSync("../ssl/cert.pem")
   });
-} else {
-  server = createHttpServer();
-}
+} else server = createHttpServer();
 
-// Handle request for `/game` before reaching `serve-static` middleware
 server.on("request", (req, res) => {
-  if (req.url === '/game') {
-    const filePath = path.join(process.cwd(), '../static/games.html');
-    res.sendFile(filePath);
-    return; // Stop processing further requests if a match is found
-  }
-
-  // If no matching route is found, let the `serve-static` middleware handle the request
-  if (bare.shouldRoute(req)) bare.routeRequest(req, res); else {
+  if(bare.shouldRoute(req)) bare.routeRequest(req, res); else {
     serve(req, res, (err) => {
       res.writeHead(err?.statusCode || 500, null, {
         "Content-Type": "text/plain",
       });
-      res.end('Error');
-    });
+      res.end('Error')
+    })
   }
 });
 
 server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
+  if(bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
 });
 
 server.on('listening', () => {
   const addr = server.address();
 
-  console.log(`Server running on port ${addr.port}`);
-  console.log('');
-  console.log('You can now view it in your browser.');
+  console.log(`Server running on port ${addr.port}`)
+	console.log('');
+  console.log('You can now view it in your browser.')
   /* Code for listing IPS from website-aio */
-
-
-  
-console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
-  try {
-    console.log(`On Your Network: http://${address.ip()}:${addr.port}`);
-  } catch (err) { /* Can't find LAN interface */ };
-  if (process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
+	console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
+	try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
+	if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
 });
 
-server.listen({ port: (process.env.PORT || 8080) });
+server.listen({ port: (process.env.PORT || 8080) })
