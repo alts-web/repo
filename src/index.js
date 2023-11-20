3e1a3060ee4be9b6f1df061fe625b72af1986076
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-11-20 01:06:06 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import createBareServer from '@tomphttp/bare-server-node';
-import { fileURLToPath } from "url";
-import { createServer as createHttpsServer } from "node:https";
-import { createServer as createHttpServer } from "node:http";
-import { readFileSync, existsSync } from "node:fs";
-import serveStatic from "serve-static";
+import createBareServer from
+ 
+'@tomphttp/bare-server-node';
+import { fileURLToPath } from
+ 
+"url";
+import { createServer as createHttpsServer } from
+ 
+"node:https";
+import { createServer as createHttpServer } from
+ 
+"node:http";
+import { readFileSync, existsSync } from
+ 
+"node:fs";
+import serveStatic from
+ 
+"serve-static";
 
 // The following message MAY NOT be removed
 console.log("Hypertabs\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
@@ -18,14 +30,17 @@ if(existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
   });
 } else server = createHttpServer();
 
+// Handle request for `/home`
 server.on("request", (req, res) => {
-  if(bare.shouldRoute(req)) bare.routeRequest(req, res); else {
+  if (req.url === '/home') {
+    res.sendFile(fileURLToPath(new URL("../static/index.html", import.meta.url)));
+  } else if (bare.shouldRoute(req)) bare.routeRequest(req, res); else {
     serve(req, res, (err) => {
       res.writeHead(err?.statusCode || 500, null, {
         "Content-Type": "text/plain",
       });
-      res.end('Error')
-    })
+      res.end('Error');
+    });
   }
 });
 
@@ -36,13 +51,15 @@ server.on("upgrade", (req, socket, head) => {
 server.on('listening', () => {
   const addr = server.address();
 
-  console.log(`Server running on port ${addr.port}`)
-	console.log('');
-  console.log('You can now view it in your browser.')
+  console.log(`Server running on port ${addr.port}`);
+  console.log('');
+  console.log('You can now view it in your browser.');
   /* Code for listing IPS from website-aio */
-	console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
-	try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
-	if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
+
+  
+console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
+  try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
+  if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
 });
 
-server.listen({ port: (process.env.PORT || 8080) })
+server.listen({ port: (process.env.PORT || 8080) });
