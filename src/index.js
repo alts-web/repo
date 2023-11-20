03a6473ba32e33026c5605217cbde853909200c7
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-11-20 02:14:46 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,44 +11,38 @@ console.log("Hypertabs\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is
 const bare = createBareServer("/bare/");
 const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), { fallthrough: false });
 var server;
-if (existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
+if(existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
   server = createHttpsServer({
     key: readFileSync("../ssl/key.pem"),
-    cert: readFileSync("../ssl/cert.pem"),
+    cert: readFileSync("../ssl/cert.pem")
   });
 } else server = createHttpServer();
 
 server.on("request", (req, res) => {
-  if (req.url === '/home') {
-    // Handle redirection to `/index.html` for `/home` requests
-    res.sendFile(fileURLToPath(new URL("../static/index.html", import.meta.url)));
-  } else if (bare.shouldRoute(req)) bare.routeRequest(req, res); else {
+  if(bare.shouldRoute(req)) bare.routeRequest(req, res); else {
     serve(req, res, (err) => {
       res.writeHead(err?.statusCode || 500, null, {
         "Content-Type": "text/plain",
       });
-      res.end('Error');
-    });
+      res.end('Error')
+    })
   }
 });
 
 server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
+  if(bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
 });
 
 server.on('listening', () => {
   const addr = server.address();
 
-  console.log(`Server running on port ${addr.port}`);
-  console.log('');
-  console.log('You can now view it in your browser.');
+  console.log(`Server running on port ${addr.port}`)
+	console.log('');
+  console.log('You can now view it in your browser.')
   /* Code for listing IPS from website-aio */
-
-  console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
-  try {
-    console.log(`On Your Network: http://${address.ip()}:${addr.port}`);
-  } catch (err) { /* Can't find LAN interface */ };
-  if (process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
+	console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
+	try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
+	if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
 });
 
-server.listen({ port: (process.env.PORT || 8080) });
+server.listen({ port: (process.env.PORT || 8080) })
