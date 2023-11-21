dc26cca2ad07dd0de0d322d7ff824a650661aa9a
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2023-11-21 00:41:33 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,91 @@
-import createBareServer from '@tomphttp/bare-server-node';
-import { fileURLToPath } from "url";
-import { createServer as createHttpsServer } from "node:https";
-import { createServer as createHttpServer } from "node:http";
-import { readFileSync, existsSync } from "node:fs";
-import serveStatic from "serve-static";
+import createBareServer from
+ 
+'@tomphttp/bare-server-node';
+import { fileURLToPath } from
+ 
+"url";
+import { createServer as createHttpsServer } from
+ 
+"node:https";
+import { createServer as createHttpServer } from
+ 
+"node:http";
+import { readFileSync, existsSync } from
+ 
+"node:fs";
+import { resolve } from "path";
+import express from 'express';
 
-// The following message MAY NOT be removed
-console.log("Hypertabs\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
+const { __dirname } = fileURLToPath(new URL('.', import.meta.url));
 
 const bare = createBareServer("/bare/");
-const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), { fallthrough: false });
+const serveStatic = express.static(resolve(__dirname, "../static"));
 var server;
-if(existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
-  server = createHttpsServer({
-    key: readFileSync("../ssl/key.pem"),
-    cert: readFileSync("../ssl/cert.pem")
-  });
-} else server = createHttpServer();
 
-server.on("request", (req, res) => {
-  if(bare.shouldRoute(req)) bare.routeRequest(req, res); else {
-    serve(req, res, (err) => {
-      res.writeHead(err?.statusCode || 500, null, {
-        "Content-Type": "text/plain",
-      });
-      res.end('Error')
-    })
+if (existsSync(resolve(__dirname, "../ssl/key.pem")) && existsSync(resolve(__dirname, "../ssl/cert.pem"))) {
+  server = createHttpsServer({
+    key: readFileSync(resolve(__dirname, "../ssl/key.pem")),
+    cert: readFileSync(resolve(__dirname, "../ssl/cert.pem")),
+  });
+} else {
+  server = createHttpServer();
+}
+
+const app = express();
+app.use(serveStatic);
+
+const routes = [
+  { path: '/', file: 'index.html' },
+  { path: '/news', file: 'apps.html' },
+  { path: '/events', file: 'games.html' },
+  { path: '/diagnostic', file: 'settings.html' },
+  { path: '/local-news', file: 'tabs.html' },
+  { path: '/image-galleries', file: 'go.html' },
+];
+
+routes.forEach((route) => {
+  app.get(route.path, (req, res) => {
+    res.sendFile(resolve(__dirname, `../static/${route.file}`));
+  });
+});
+
+server.on('request', (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
   }
 });
 
-server.on("upgrade", (req, socket, head) => {
-  if(bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
+server.on('upgrade', (req, socket, head) => {
+  if (bare.shouldRoute(req, socket, head)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
 });
 
 server.on('listening', () => {
   const addr = server.address();
 
-  console.log(`Server running on port ${addr.port}`)
-	console.log('');
-  console.log('You can now view it in your browser.')
+  console.log(`Server running on port ${addr.port}`);
+  console.log('');
+  console.log('You can now view it in your browser.');
+
   /* Code for listing IPS from website-aio */
-	console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
-	try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
-	if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
+
+  
+console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
+
+  try {
+    console.log(`On Your Network: http://${address.ip()}:${addr.port}`);
+  } catch (err) {
+    /* Can't find LAN interface */
+  }
+
+  if (process.env.REPL_SLUG && process.env.REPL_OWNER) {
+    console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
+  }
 });
 
-server.listen({ port: (process.env.PORT || 8080) })
+server.listen({ port: (process.env.PORT || 8080) });
