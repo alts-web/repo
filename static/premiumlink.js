8152fb0162c99e409646a49112953d7a848e1491
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2024-01-17 10:12:13 -0600

    Update premiumlink.js

--- static/premiumlink.js
+++ static/premiumlink.js
@@ -3,7 +3,7 @@
         if (password && password === validPassword) {
             document.body.style.display = 'block';
         } else {
-            var input = prompt('Enter The Password For This Premium Link:');
+            var input = prompt('This Premium Link Is Password Protected. Enter The Password:');
             if (input === validPassword) {
                 window.localStorage.setItem('password', input);
                 document.body.style.display = 'block';
